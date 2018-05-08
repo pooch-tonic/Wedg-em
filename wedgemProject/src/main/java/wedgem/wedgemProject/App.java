e2b9e07d0ceff784e22b5e5c080090767e44f18f
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-05-08 18:18:32 +0200

    maven done

---
+++ wedgemProject/src/main/java/wedgem/wedgemProject/App.java
@@ -0,0 +1,13 @@
+package wedgem.wedgemProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
