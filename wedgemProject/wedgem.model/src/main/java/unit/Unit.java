79b71148878021555defa2c3344b956e6f7c82a8
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-05-08 19:11:20 +0200

    Unit done

--- wedgemProject/wedgem.model/src/main/java/unit/Unit.java
+++ wedgemProject/wedgem.model/src/main/java/unit/Unit.java
@@ -1,11 +1,34 @@
 package unit;
 
+import vector.Vector;
+
 public class Unit {
+	private int		player;
+	private Vector	position;
 
 	public Unit() {
+		this.setPlayer(0);
+		this.setPosition(null);
+	}
+
+	public Unit(final int player, final Vector position) {
 
 	}
 
-	public Unit(final int player, )
+	public int getPlayer() {
+		return this.player;
+	}
+
+	public Vector getPosition() {
+		return this.position;
+	}
+
+	public void setPlayer(final int player) {
+		this.player = player;
+	}
+
+	public void setPosition(final Vector position) {
+		this.position = position;
+	}
 
 }
