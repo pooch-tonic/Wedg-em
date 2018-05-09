54c3b5576ab28fb9e6df982c9fbf43e807aed414
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-05-09 16:06:39 +0200

    update

---
+++ wedgemProject/wedgem.contract/src/main/java/interfacesModel/IUnit.java
@@ -0,0 +1,13 @@
+package interfacesModel;
+
+import vector.Vector;
+
+public interface IUnit {
+	public int getPlayer();
+
+	public Vector getPosition();
+
+	public void setPosition();
+
+	public void wedge();
+}
