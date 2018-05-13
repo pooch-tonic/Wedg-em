8c1732d44b6ec2e24f5f9f46f7bd3d8b549c69a2
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-05-13 23:47:21 +0200

    backup commit before test build

--- wedgemProject/wedgem.main/src/main/java/wedgem/main/WedgemGame.java
+++ wedgemProject/wedgem.main/src/main/java/wedgem/main/WedgemGame.java
@@ -1,13 +1,28 @@
 package wedgem.main;
 
+import Window.Frame;
+
 public class WedgemGame {
+	Frame frame;
 
 	public WedgemGame() {
-		// TODO constructor
+		this.initWedgemGame();
+	}
+
+	public Frame getFrame() {
+		return this.frame;
+	}
+
+	private void initWedgemGame() {
+		this.setFrame(new Frame());
 	}
 
 	public void play() {
 		// TODO play method
+	}
+
+	public void setFrame(final Frame frame) {
+		this.frame = frame;
 	}
 
 }
---
+++ wedgemProject/wedgem.view/src/main/java/Window/Panel.java
@@ -0,0 +1,9 @@
+package Window;
+
+public class Panel {
+
+	public Panel() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
