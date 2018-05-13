5c366b920e5e803acb28c0b5749a815c02d0cff4
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-05-14 00:37:11 +0200

    updated panel

--- wedgemProject/wedgem.view/src/main/java/Window/Panel.java
+++ wedgemProject/wedgem.view/src/main/java/Window/Panel.java
@@ -1,18 +1,26 @@
 package Window;
 
+import java.awt.Image;
+
 import javax.swing.JPanel;
 
 import interfacesModel.IBoard;
 
 public class Panel extends JPanel {
+	private static final long	serialVersionUID	= 6527399859189638479L;
 
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = 6527399859189638479L;
+	private Image				boardSprite;
 
 	public Panel(final IBoard board) {
-		// TODO Auto-generated constructor stub
+		this.setBoardSprite(board.getSprite());
+	}
+
+	public Image getBoardSprite() {
+		return this.boardSprite;
+	}
+
+	public void setBoardSprite(final Image boardSprite) {
+		this.boardSprite = boardSprite;
 	}
 
 }
