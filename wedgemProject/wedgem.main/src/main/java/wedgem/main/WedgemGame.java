1490765c8a68efa01d04fa338de5a93cbbdb0a23
Ryo SHIINA <pooch-tonic@users.noreply.github.com>
2018-05-14 00:54:38 +0200

    backup commit before test run

--- wedgemProject/wedgem.main/src/main/java/wedgem/main/WedgemGame.java
+++ wedgemProject/wedgem.main/src/main/java/wedgem/main/WedgemGame.java
@@ -1,9 +1,12 @@
 package wedgem.main;
 
 import Window.Frame;
+import game.Game;
+import interfacesModel.IGame;
 
 public class WedgemGame {
-	Frame frame;
+	private Frame	frame;
+	private IGame	game;
 
 	public WedgemGame() {
 		this.initWedgemGame();
@@ -13,8 +16,13 @@ public class WedgemGame {
 		return this.frame;
 	}
 
+	private IGame getGame() {
+		return this.game;
+	}
+
 	private void initWedgemGame() {
-		this.setFrame(new Frame());
+		this.setFrame(new Frame(this.getGame().getBoard()));
+		this.setGame(new Game());
 	}
 
 	public void play() {
@@ -23,6 +31,10 @@ public class WedgemGame {
 
 	public void setFrame(final Frame frame) {
 		this.frame = frame;
+	}
+
+	private void setGame(final IGame game) {
+		this.game = game;
 	}
 
 }
